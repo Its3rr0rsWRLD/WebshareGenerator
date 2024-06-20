4acf06b82771d9885e1b8af33c3849293221e9c8
Garry <168931296+garrydevpro@users.noreply.github.com>
2024-06-20 12:35:53 -0400

    improve code

--- generator.py
+++ generator.py
@@ -1,7 +1,5 @@
 import os
 
-os.system("pip install -r requirements.txt")
-
 if os.name == 'nt':
     os.system("cls")
 else:
@@ -37,11 +35,11 @@ def update():
     except Exception as e:
         print(f"An error occurred while checking for updates: {e}")
 
-def config():
+def create_config():
     print("Proxyless? [Recommended] (y/n): ", end="")
     proxyless = input().strip().lower()
     print("Captcha API Key: ", end="")
-    captcha_key = input().strip()
+    captcha_apikey = input().strip()
     print("Captcha Service (capmonster/capsolver): ", end="")
     captcha_service = input().strip().lower()
     print("Proxy File? [Leave Blank If Proxyless] (filename): ", end="")
@@ -51,7 +49,7 @@ def config():
 
     data = {
         "proxyless": proxyless == 'y',
-        "captcha_apikey": captcha_key,
+        "captcha_apikey": captcha_apikey,
         "captcha_service": captcha_service,
         "proxy_file": proxy_file,
         "threads": threads
@@ -62,18 +60,19 @@ def config():
 
     print("Config saved. To edit the config, open config.json")
 
-def worker(proxyless, captcha_key, captcha_service, proxies):
-    webshare = Webshare(proxyless=proxyless, captcha_key=captcha_key, captcha_service=captcha_service)
-    webshare.begin()
+def worker(proxyless, captcha_apikey, captcha_service, proxies):
+    webshare = Webshare(proxyless, captcha_apikey, captcha_service, proxies)
+    while True:
+        webshare.generate_proxies()
 
 def main():
     if not os.path.exists("config.json"):
-        config()
+        create_config()
 
     with open("config.json") as f:
         data = json.load(f)
         proxyless = data.get("proxyless")
-        captcha_key = data.get("captcha_apikey")
+        captcha_apikey = data.get("captcha_apikey")
         captcha_service = data.get("captcha_service")
         proxy_file = data.get("proxy_file")
         threads = data.get("threads")
@@ -84,9 +83,11 @@ def main():
     else:
         proxies = []
 
+    print("You can stop generation by pressing CTRL + C\n")
+
     thread_list = []
     for _ in range(threads):
-        thread = threading.Thread(target=worker, args=(proxyless, captcha_key, captcha_service, proxies))
+        thread = threading.Thread(target=worker, args=(proxyless, captcha_apikey, captcha_service, proxies), daemon=True)
         thread_list.append(thread)
         thread.start()
 
